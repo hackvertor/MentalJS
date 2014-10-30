8ff86a464f0d454b74761e069195d056d0e23970
Gareth Heyes <gazheyes@gmail.com>
2014-10-30 20:12:36 +0000

    Added php demo for content injection

---
+++ scripts/content_injection.php
@@ -0,0 +1,14 @@
+<!doctype HTML>
+<html>
+	<head>
+		<meta charset="UTF-8" />
+		<title>Content injection demo</title>
+		<script src="../javascript/Mental.js"></script>
+		<script src="content_injection.js"></script>
+	</head>
+	<body>	
+		<plaintext id="MentalRender" />							
+		<h1>Content Injection demo</h1>
+		<?php echo $_GET['x']?>
+	</body>
+</html>
