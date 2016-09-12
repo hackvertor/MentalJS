3f32c764ecf3779d4172296815b4f20a212576c1
hackvertor <gazheyes@gmail.com>
2016-09-12 21:47:41 +0100

    Content injection demo
    Updated content injection demo and added onerror in allowed attributes
    for DOMPurify

--- scripts/content_injection.js
+++ scripts/content_injection.js
@@ -1,6 +1,59 @@
 window.addEventListener('DOMContentLoaded', function() {        
        var html = document.getElementById('MentalRender').textContent, js=MentalJS();
-       js.init({dom:true});
+       js.init({
+            dom:true,
+            parseInnerHTML : function(dirty) {
+                var config = {
+                    ADD_TAGS: ['script'],
+                    ADD_ATTR: ['onclick', 'onmouseover','onerror']
+                };
+                DOMPurify.addHook('uponSanitizeElement', function(node, data) {
+                    if (data.tagName === 'script') {
+                        var script = node.textContent;
+                        if (!script || 'src' in node.attributes
+                            || 'href' in node.attributes
+                            || 'xlink:href' in node.attributes) {
+                                return node.parentNode.removeChild(node)
+                        }
+                        try {
+                            // Pass scripts to MentalJS
+                            var mental = MentalJS().parse(
+                                {
+                                    options: {
+                                        eval: false,
+                                        dom:true
+                                    },
+                                 code:script
+                                }
+                            );
+                            return node.textContent = mental;
+                        } catch (e) {
+                            return node.parentNode.removeChild(node);
+                        }
+                    }
+                });
+                DOMPurify.addHook('uponSanitizeAttribute', function(node, data) {
+                    if (data.attrName.match(/^on\w+/)) {
+                        var script = data.attrValue;
+                        try {
+                            // Pass scripts to MentalJS
+                            return data.attrValue = MentalJS().parse(
+                                {
+                                    options: {
+                                        eval: false,
+                                        dom: true
+                                    },
+                                    code: script
+                                }
+                            );
+                        } catch (e) {
+                            return data.attrValue = '';
+                        }
+                    }
+                });
+                return DOMPurify.sanitize(dirty, config);
+            }
+       });
        js.parse({options:{eval:true},code:'this.sandboxedFunction=function(str){document.body.innerHTML=str}'});
        this.sandboxedFunction$(html);
 }, false);
--- scripts/content_injection.php
+++ scripts/content_injection.php
@@ -3,11 +3,12 @@
 	<head>
 		<meta charset="UTF-8" />
 		<title>Content injection demo</title>
+		<script src="../javascript/purify.js"></script>
 		<script src="../javascript/Mental.js"></script>
 		<script src="content_injection.js"></script>
 	</head>
-	<body>	
-		<plaintext id="MentalRender" />							
+	<body>
+		<plaintext id="MentalRender" />
 		<h1>Content Injection demo</h1>
 		<?php echo $_GET['x']?>
 	</body>
